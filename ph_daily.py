300eb82da9b95876195db99c7c9ce26bf2fc0fda
iStar <istar309@gmail.com>
2020-11-25 21:25:48 +0800

    Use product hunt API

--- ph_daily.py
+++ ph_daily.py
@@ -24,11 +24,14 @@ passw = os.environ.get('MEMCACHIER_PASSWORD', '')
 mc = bmemcached.Client(servers, username=user, password=passw)
 mc.enable_retry_delay(True)  # Enabled by default. Sets retry delay to 5s.
 
+#product hunt
+developer_token = os.environ.get('PH_TOKEN', '')
 
 def fetch_posts():
-    url = 'https://ph-graph-api-explorer.herokuapp.com/graphql'
+    url = 'https://api.producthunt.com/v2/api/graphql'
+    headers = {'Authorization': 'Bearer {}'.format(developer_token)}
     data = {'query':'{\n  posts(first: 5) {\n    edges {\n      node {\n        id\n        name\n        description\n        url\n        votesCount\n    \t\ttopics {\n    \t\t  edges {\n    \t\t    node {\n              name\n    \t\t    }\n    \t\t  }\n    \t\t}\n      }\n    }\n  }\n}\n'}
-    r = requests.post(url, json = data)
+    r = requests.post(url, json = data, headers=headers)
     result = r.json()
     if result is None:
         print('Something goes wrong when fetch posts!')
